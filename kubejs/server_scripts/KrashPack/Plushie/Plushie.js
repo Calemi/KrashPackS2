86f2f13f63fd3a26a90e07ff319278eb041772a2
Ashley123477 <ashleyokeeffe1998@outlook.com>
2025-03-09 22:01:12 +0000

    Delete

--- kubejs/server_scripts/KrashPack/Plushie/Plushie.js
+++
@@ -1,6 +0,0 @@
-ServerEvents.recipes(event => {
-    // Remove recipes from the Plushies mod
-    event.remove({ id: 'plushies:zombie_plushie' });
-	event.remove({ id: 'plushies:skeleton_plushie' });
-
-});
